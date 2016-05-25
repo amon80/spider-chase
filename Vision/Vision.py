56591b4873041bd5de59a12b2e060e06f5f5b32d
Abelarm <luigi3000@gmail.com>
2016-05-25 19:52:34 +0200

    I dont know what i'm doing

--- Vision/Vision.py
+++ Vision/Vision.py
@@ -2,6 +2,9 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+import subprocess
+import time
+from multiprocessing import Process
 
 class Vision:
 	
@@ -39,7 +42,7 @@ class Vision:
 	def get_Spider(self, color1Lower, color1Upper, color2Lower, color2Upper):
 
 		(grabbed, frame) = self.camera.read()
-		frame = cv2.flip(frame,1)
+		#frame = cv2.flip(frame,1)
 		s_x = None
 		s_y = None 
 		f_x = None 
@@ -129,6 +132,69 @@ class Vision:
 			p1 = np.array([s_x,s_y])
 			return self.calculate_matrix(p0,p1)
 
+	def get_Spider_Inseguitore(self, color1Lower, color1Upper):
+
+		(grabbed, frame) = self.camera.read()
+		#frame = cv2.flip(frame,1)
+		s_x = None
+		s_y = None 
+		f_x = None 
+		f_y = None
+		# if we are viewing a video and we did not grab a frame,
+		# then we have reached the end of the video
+		if not grabbed:
+			print("No frame D:")
+
+		# resize the frame, blur it, and convert it to the HSV
+		# color space
+		frame = imutils.resize(frame, width=900)
+		blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+		#cv2.imshow("Blurred", blurred)
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		cv2.imshow("hsv", hsv)
+
+		# construct a mask for the color "green", then perform
+		# a series of dilations and erosions to remove any small
+		# blobs left in the mask
+		mask = cv2.inRange(hsv, color1Lower, color1Upper)
+		mask = cv2.erode(mask, None, iterations=3)
+		mask = cv2.dilate(mask, None, iterations=2)
+
+		cv2.imshow("mask1",cv2.bitwise_and(hsv,hsv,mask=mask));
+
+		# find contours in the mask and initialize the current
+		# (x, y) center of the ball
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+		center = None
+		# only proceed if at least one contour was found
+		if len(cnts) > 0:
+			# find the largest contour in the mask, then use
+			# it to compute the minimum enclosing circle and
+			# centroid
+			c = max(cnts, key=cv2.contourArea)
+			((f_x, f_y), radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+			# only proceed if the radius meets a minimum size
+			if radius > 5:
+				# draw the circle and centroid on the frame,
+				# then update the list of tracked points
+				cv2.circle(frame, (int(f_x), int(f_y)), int(radius),
+				(0, 255, 255), 2)
+				cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+
+		
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("q"):
+			close_all()
+
+		if f_x and f_y:
+			p1 = np.array([f_x, f_y])
+			return p1
 
 	#dati due punti restituisce la matrice omogenea di rototraslazione
 	def calculate_matrix(self, p0, p1, versor=[1,0]):
@@ -154,19 +220,92 @@ class Vision:
 		self.camera.release()
 		cv2.destroyAllWindows()
 
+def launch_curl(string):
+	subprocess.call("curl -m 1 http://192.168.4.1/?c=m0"+string, shell=True)
+
 if __name__ == '__main__':
 
 	g = Vision()
 	while True:
 		first_matrix = g.get_Spider(g.f_s_c1Lower, g.f_s_c1Upper, g.f_s_c2Lower, g.f_s_c2Upper)
-		second_matrix = g.get_Spider(g.s_s_c1Lower, g.s_s_c1Upper, g.s_s_c2Lower, g.s_s_c2Upper)
+		point = g.get_Spider_Inseguitore(g.s_s_c1Lower, g.s_s_c1Upper)
 
-		if not first_matrix==None and not second_matrix==None:
+		if not first_matrix == None and not point == None :
 			#print(first_matrix, second_matrix)
 
+			#print(first_matrix[0][0])
+
 			p0 = [first_matrix[0][2], first_matrix[1][2]]
-			p1 = [second_matrix[0][2], second_matrix[1][2]]
-			new_matrix_x= g.calculate_matrix(np.array(p0),np.array(p1))
-			new_matrix_y= g.calculate_matrix(np.array(p0),np.array(p1),[0,1])
+			p1 = point
+
+			new_matrix_x= g.calculate_matrix(np.array(p0), p1)
+			new_matrix_y= g.calculate_matrix(np.array(p0), p1, [0,1])
+
 			cos_x = new_matrix_x[0][0]
 			cos_y = new_matrix_y[0][0]
+
+			#print("Coseno rispetto X: "+ str(cos_x))
+			#print("Coseno rispetto Y: "+ str(cos_y))
+
+			epsilon_rot = 0.5
+			epsilon_fron = 0.5
+			#oggetto a destra
+
+			diff = np.linalg.norm(p1-p0, ord = 2)
+			print(diff)
+			if diff < 150:
+				print("FERMATIIIIIIIII")
+				p = Process(target=launch_curl,args=('128128', ))
+				p.start()
+				continue
+
+			print(first_matrix[0][0])
+
+			if abs(first_matrix[0][0]) <= epsilon_rot:
+
+				if cos_x > epsilon_rot:
+					print("Vai a Sinistra____1")
+					#p = Process(target=launch_curl,args=('000255', ))
+					#p.start()
+					continue
+				elif cos_x < -epsilon_rot:
+					print("Vai a Destra____1")
+					#p = Process(target=launch_curl,args=('255000', ))
+					#p.start()
+					continue
+					
+				if cos_y > epsilon_fron:
+					#p = Process(target=launch_curl,args=('000000', ))
+					#p.start()
+					print("Vai a Avanti_____1")
+				elif cos_y < -epsilon_fron:
+					#p = Process(target=launch_curl,args=('255255', ))
+					#p.start()
+					print("Vai a Indietro____1")
+					
+			else:
+
+				if cos_x > epsilon_rot:
+					print("Vai a Avanti_____2")
+					#p = Process(target=launch_curl,args=('255255', ))
+					#p.start()
+					continue
+				elif cos_x < -epsilon_rot:
+					print("Vai a Indietro______2")
+					#p = Process(target=launch_curl,args=('000000', ))
+					#p.start()
+					continue
+				
+				if cos_y > epsilon_fron:
+					#p = Process(target=launch_curl,args=('000255', ))
+					#p.start()
+					print("Vai a Destra____2")
+				elif cos_y < -epsilon_fron:
+					#p = Process(target=launch_curl,args=('255000', ))
+					#p.start()
+					print("Vai a Sinistra____2")
+
+
+
+
+
